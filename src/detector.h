3bdfad78d446ca0ff5c47d48f5ea86d3950bcd1d
Justin Fung <qf28@sussex.ac.uk>
2024-01-28 20:15:11 +0000

    Add detector struct

--- src/detector.h
+++ src/detector.h
@@ -3,9 +3,29 @@
 typedef struct
 {
     int x1;
-    int x2;
     int y1;
+    int x2;
     int y2;
     int label;
     float prob
 } BoxInfo;
+
+typedef struct
+{
+    ncnn_net_t net;
+    int input_w;
+    int input_h;
+    BoxInfo (*detect)(unsigned char);
+} Detector;
+
+/**
+ * Todo -- General function that share with all detector
+ */
+
+/**
+ * Todo -- Nanodet's widget
+ */
+
+/**
+ * Todo -- FastestDet's widget
+ */
