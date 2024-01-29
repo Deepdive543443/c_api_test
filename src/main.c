76b7738ecd95340ab8dcb0eea41665e068702111
Justin Fung <qf28@sussex.ac.uk>
2024-01-29 01:17:36 +0000

    clean up

--- src/main.c
+++ src/main.c
@@ -23,22 +23,22 @@ void print_mat(ncnn_mat_t *mat)
 
 int main(int argc, char** argv)
 {
-    union 
-    {
-        struct
-        {
-            __uint8_t r;
-            __uint8_t g;
-            __uint8_t b;
-            __uint8_t a;
-        };
-        __uint32_t rgba;
-    } color;
+    // union 
+    // {
+    //     struct
+    //     {
+    //         __uint8_t r;
+    //         __uint8_t g;
+    //         __uint8_t b;
+    //         __uint8_t a;
+    //     };
+    //     __uint32_t rgba;
+    // } color;
 
-    color.r = 216;
-    color.g = 82;
-    color.b = 24;
-    color.a = 255;
+    // color.r = 216;
+    // color.g = 82;
+    // color.b = 24;
+    // color.a = 255;
 
     int target_size = 416;
 
@@ -60,12 +60,13 @@ int main(int argc, char** argv)
      * 
      */
     Detector nanodet = create_nanodet(target_size);
-    BoxInfo *objects = NULL;
+    BoxInfo *objects = NULL; // Dynamic array doesn't work for now, waiting for an implementation
     nanodet.detect(pixels, width, height, objects, &nanodet);
     free(pixels);
+    
 
-    BoxInfo obj = objects[0];
-    printf("%f %f %f %f %f %d\n", objects[0].x1, objects[0].x2, objects[0].y1, objects[0].y2, objects[0].prob, objects[0].label);
+    // BoxInfo obj = objects[0];
+    // printf("%f %f %f %f %f %d\n", objects[0].x1, objects[0].x2, objects[0].y1, objects[0].y2, objects[0].prob, objects[0].label);
 
 
     // BoxInfo *boxxes = NULL;
